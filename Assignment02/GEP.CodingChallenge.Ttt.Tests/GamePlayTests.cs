726b9d3d9028bb383f2810abb7f405110f677623
Eduard <e.keilholz@4dotnet.nl>
2020-10-22 14:18:10 +0200

    Assignment II

---
+++ Assignment02/GEP.CodingChallenge.Ttt.Tests/GamePlayTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using GEP.CodingChallenge.Ttt.Domain;
+using GEP.CodingChallenge.Ttt.Exceptions;
+using NUnit.Framework;
+
+namespace GEP.CodingChallenge.Ttt.Tests
+{
+    public class GamePlayTests
+    {
+
+        [Test]
+        public void WhenACoordinateIsAlreadyOccupied_ItThrowsCoordinateAlreadyOccupiedException()
+        {
+            var game = new Game();
+            var occupations = new List<Coordinate>();
+            occupations.Add(new Coordinate(1, 1, game.PlayerOne));
+            occupations.Add(new Coordinate(1, 1, game.PlayerTwo));
+            var act = new TestDelegate(() => game.Initialize(occupations));
+            Assert.Throws<CoordinateAlreadyOccupiedException>(act);
+        }
+
+    }
+}
---
+++ Assignment02/GEP.CodingChallenge.Ttt/Domain/Game.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using GEP.CodingChallenge.Ttt.Base;
+
+namespace GEP.CodingChallenge.Ttt.Domain
+{
+    public sealed class Game
+    {
+        private Matrix _gameMatrix;
+
+        public PlayerOne PlayerOne { get; private set; }
+        public PlayerTwo PlayerTwo { get; private set; }
+
+        public bool HasWinner => _gameMatrix.HasWinner;
+        public bool HasEnded => _gameMatrix.CountTotalMoves() == Constants.MatrixSize * Constants.MatrixSize;
+
+        public Player GetWinner()
+        {
+            return _gameMatrix.GetWinner();
+        }
+
+        public Player GetPlayerWhoHasTurn()
+        {
+            var playerOneMoves = _gameMatrix.CountPlayerMoves(PlayerOne);
+            var playerTwoMoves = _gameMatrix.CountPlayerMoves(PlayerTwo);
+            return playerOneMoves > playerTwoMoves ? (Player)PlayerTwo: PlayerOne;
+        }
+
+        public string GetStatusText()
+        {
+            var player = GetWinner() ?? GetPlayerWhoHasTurn();
+            if (HasEnded && !HasWinner)
+            {
+                return "This game ended up in a tie... ;(";
+            }
+            var statusText = HasWinner ? $"{player.Name} is the winner!" : $"It's {player.Name}'s turn";
+            return statusText;
+        }
+
+
+        public void Initialize(IEnumerable<Coordinate> occupations)
+        {
+            foreach (var occ in occupations)
+            {
+                _gameMatrix.Occupy(occ.CoordX, occ.CoordY, occ.Occupation);
+            }
+        }
+
+        public Game(string playerOneName = null, string playerTwoName = null)
+        {
+            PlayerOne = new PlayerOne(playerOneName);
+            PlayerTwo = new PlayerTwo(playerTwoName);
+            _gameMatrix = new Matrix();
+        }
+    }
+}
